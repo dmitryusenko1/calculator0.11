6719463baabe72f3dd1a4380c6fb5edce6bc7406
dmitryusenko1 <100082547+dmitryusenko1@users.noreply.github.com>
2022-04-30 19:27:33 +0300

    calculator0.1.py

---
+++ sandbox.py
@@ -0,0 +1,19 @@
+from ctb import place
+
+for x in range(10):
+    place(x, 0, 0)
+    place(x, 0, 1)
+    place(x, 0, 2)
+    place(x, 0, 3)
+    place(x, 0, 4)
+    place(x, 0, 5)
+    place(x, 0, 6)
+    place(x, 0, 7)
+    place(x, 0, 8)
+    place(x, 0, 9)
+    place(x, 0, 10)
+
+for z in range(10):
+    place(0, 0, z)
+
+
